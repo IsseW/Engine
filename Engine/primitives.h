368f0e491cdcb4f58a31f08f1c34d2aa82eb400b
IsseW <isidornie@gmail.com>
2022-06-06 22:41:25 +0200

    Primitives

---
+++ Engine/primitives.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<cstdint>
+#include<cstdlib>
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+
+using i8 = uint8_t;
+using i16 = uint16_t;
+using i32 = uint32_t;
+using i64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
