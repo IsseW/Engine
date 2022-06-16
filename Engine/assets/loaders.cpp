c01f0fc9402cfe404393b16b1f41d820bf750d52
edwin.ljung <edwin.ljung@gmail.com>
2022-06-16 17:13:01 +0200

    Model decoding

--- Engine/assets/loaders.cpp
+++ Engine/assets/loaders.cpp
@@ -6,14 +6,24 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include"stb_image.h"
 
-Image Image::load(const std::string& path, ID3D11Device* device) {
+Image Image::load(const std::string& path) {
 	int width, height, comp;
-	auto* image = stbi_load(path.data(), &width, &height, &comp, 0);
+	u8* data = stbi_load(path.data(), &width, &height, &comp, 0);
 
-	if (image == nullptr) {
+	if (data == nullptr) {
 		PANIC("Failed to load image file");
 	}
 
+	return Image{
+		data,
+		(u32)width,
+		(u32)height,
+		(u32)comp,
+		none<Binded>()
+	};
+}
+
+void Image::bind(ID3D11Device* device) {
 	D3D11_TEXTURE2D_DESC desc;
 	desc.ArraySize = 1;
 	desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
@@ -26,27 +36,27 @@ Image Image::load(const std::string& path, ID3D11Device* device) {
 	desc.SampleDesc.Count = 1;
 	desc.SampleDesc.Quality = 0;
 
-	switch (comp) {
-		case 1: {
-			desc.Format = DXGI_FORMAT_R8_UNORM;
-			break;
-		};
-		case 2: {
-			desc.Format = DXGI_FORMAT_R8G8_UNORM;
-			break;
-		};
-		case 4: {
-			desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-			break;
-		};
-		default: {
-			TODO;
-		}
+	switch (channels) {
+	case 1: {
+		desc.Format = DXGI_FORMAT_R8_UNORM;
+		break;
+	};
+	case 2: {
+		desc.Format = DXGI_FORMAT_R8G8_UNORM;
+		break;
+	};
+	case 4: {
+		desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		break;
+	};
+	default: {
+		TODO;
+	}
 	}
 
 	D3D11_SUBRESOURCE_DATA data;
-	data.pSysMem = image;
-	data.SysMemPitch = width * sizeof(unsigned char) * comp;
+	data.pSysMem = this->data;
+	data.SysMemPitch = width * sizeof(unsigned char) * channels;
 	data.SysMemSlicePitch = 0;
 
 	ID3D11Texture2D* texture;
@@ -60,14 +70,12 @@ Image Image::load(const std::string& path, ID3D11Device* device) {
 		PANIC("Failed to create shader resource view for texture.");
 	}
 
-	return Image{
-		texture,
-		rsv,
-		(usize)width,
-		(usize)height,
-		(usize)comp,
-	};
+	binded.insert(Binded{
+			texture,
+			rsv
+		});
 }
+
 
 Mesh Mesh::load(const std::string& path, ID3D11Device* device) {
 	auto file = std::ifstream{path};
