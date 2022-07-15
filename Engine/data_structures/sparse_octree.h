ab9d7e59f4b62ee142cbf08b7ebe42f560c8a910
edwin.ljung <edwin.ljung@gmail.com>
2022-07-15 12:19:12 +0200

    octree

--- Engine/data_structures/sparse_octree.h
+++ Engine/data_structures/sparse_octree.h
@@ -8,7 +8,8 @@ struct SparseOctree {
 	SparseOctree(Aabb<f32> dimension) : __inner_dimension{ dimension }, __len{0} {}
 	
 	void insert_absolute(Aabb<f32> abs, u32 index) {
-
+		auto aabbs = Aabb<f32>::split_aabb(this->__inner_dimension);
+		for (auto& aabb : aabbs) {}
 	}
 
 	void insert_relative(Aabb<f32> rel, u32 index) {
@@ -16,11 +17,15 @@ struct SparseOctree {
 	}
 
 	void remove_by_index(u32 index) {
-		
+
+	}
+private:
+	void insert(Aabb<f32> dimensions, Aabb<f32> collision) {}
+
+	static std::array<Aabb<f32>, 8> split_aabb(const Aabb<f32>& aabb) {
+	
 	}
 
-
-private:
 	Aabb<f32> __inner_dimension;
 	usize __len;
 	u32 __flat_array[DEPTH];
